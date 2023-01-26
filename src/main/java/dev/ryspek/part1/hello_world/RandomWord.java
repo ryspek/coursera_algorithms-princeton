f7cfc571011ffb4e6e417fe973e7b9ffcdfcc8b0
Ryspek Baibosunov <ryspek.baybosunov@gmail.com>
2023-01-26 12:48:45 +0600

    finish Hello world assignment

--- src/main/java/dev/ryspek/part1/hello_world/RandomWord.java
+++ src/main/java/dev/ryspek/part1/hello_world/RandomWord.java
@@ -7,22 +7,14 @@ import edu.princeton.cs.algs4.StdRandom;
 public class RandomWord {
 
     public static void main(String[] args) {
-
-        String champion = StdIn.readString();
-
-        int i = 1;
-
-        while (!StdIn.isEmpty()) {
+        String champion = "";
+        for (int i = 1; !StdIn.isEmpty(); i++) {
             String rival = StdIn.readString();
-            i++;
             if (StdRandom.bernoulli(1.0/ i)) {
                 champion = rival;
             }
         }
-
-        System.out.println(champion);
-        return;
-
+        StdOut.println(champion);
     }
 
 }
